9278daa23a2d6b8b84fae49946f155a6a23e2b0c
Ömer temel <42616536+Puyz@users.noreply.github.com>
2022-01-23 16:16:44 +0300

    Update MailManager.cs

--- Libapp/Mail/MailManager.cs
+++ Libapp/Mail/MailManager.cs
@@ -9,13 +9,13 @@ namespace Libapp.Mail
         public void SendCodeToMail(string toMail, int code, string subject)
         {
             MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("info@puyz.net", "Puyz® Bilişim");
+            mailMessage.From = new MailAddress("noname@puyz.net", "Puyz® Bilişim");
             mailMessage.To.Add(toMail);
             mailMessage.Subject = subject;
             mailMessage.Body = "Doğrulama kodun: " + code;
 
             SmtpClient smtp = new SmtpClient();
-            smtp.Credentials = new System.Net.NetworkCredential("info@puyz.net", "Omertemel123.@1");
+            smtp.Credentials = new System.Net.NetworkCredential("noname@puyz.net", "pass");
             smtp.Host = "mail.puyz.net";
             smtp.EnableSsl = true;
             smtp.Port = 587;
@@ -25,13 +25,13 @@ namespace Libapp.Mail
         public void SendPasswordToMail(string toMail, string password, string subject)
         {
             MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("info@puyz.net", "Puyz® Bilişim");
+            mailMessage.From = new MailAddress("noname@puyz.net", "Puyz® Bilişim");
             mailMessage.To.Add(toMail);
             mailMessage.Subject = subject;
             mailMessage.Body = "Email: " + toMail+"\nŞifre: "+password;
 
             SmtpClient smtp = new SmtpClient();
-            smtp.Credentials = new System.Net.NetworkCredential("info@puyz.net", "Omertemel123.@1");
+            smtp.Credentials = new System.Net.NetworkCredential("noname@puyz.net", "pass");
             smtp.Host = "mail.puyz.net";
             smtp.EnableSsl = true;
             smtp.Port = 587;
